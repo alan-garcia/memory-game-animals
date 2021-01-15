11b4e64d0321d599810dcc5cedac2e3c6a38a07f
alan-garcia <alangarcianavarro@hotmail.com>
2021-01-15 13:21:27 +0100

    Mostrar pareja de animales aleatorias sin repetir

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 "use strict";
+
+let imagesConfig = {
+  path: "images",
+  extension: ".jpg"
+}
+
+let images = ["leon", "elefante", "vaca"];
+let imagesCopy = [...images];
+let imagesFilled = [];
+
+let cells = document.querySelectorAll(".grid-animals-cell");
+cells.forEach((cell) => {
+  let randomImagePosition = Math.floor(Math.random() * imagesCopy.length);
+  let imageName = imagesCopy[randomImagePosition];
+
+  cell.innerHTML = `<img src='${ imagesConfig.path }/${ imageName }${ imagesConfig.extension}'/>`;
+  imagesFilled.push(imageName);
+
+  let numberOfCurrentAnimalOnBoard = imagesFilled.filter(animalName => animalName === imageName).length;
+  if (numberOfCurrentAnimalOnBoard === 2) {
+    imagesCopy = imagesCopy.filter(image => image !== imageName);
+  }
+});
