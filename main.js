dc6f4d59011467c0ffd38f961f1ebf27fe39af6e
alan-garcia <alangarcianavarro@hotmail.com>
2021-01-19 14:43:58 +0100

    Diseño centrado para el tablero del juego

--- main.js
+++ main.js
@@ -31,30 +31,34 @@ function deleteRepeatedAnimal(imageName) {
   return imagesCopy;
 }
 
-cells.forEach(cell => cell.addEventListener("click", selectAnimal));
+// let lockBoard = false;
+cells.forEach(cell => cell.addEventListener("click", flipCard));
 
-function selectAnimal(event) {
+function flipCard(event) {
+  // if(lockBoard) {
+  //   return;
+  // }
   let currentCell = event.currentTarget;
   let thisAnimal = currentCell.children[0];
   
   thisAnimal.style.display = "table-cell";
   imagesSelected.push(thisAnimal);
   currentCell.classList.remove("grid-animals-cell_hover");
-
   if (isCoupleSelected()) {
+    // lockboard = true;
     if (imagesSelected[0].src !== imagesSelected[1].src) {
       setTimeout(() => {
         hideFailSelectedCouple(imagesSelected);
         imagesSelected = [];
+        // lockBoard = false;
       }, 1200);
     }
     else {
       imagesSelected = [];
     }
   }
-
-  let cellsArray = Array.from(cells);
-  if (checkIfGameFinished(cellsArray)) {
+  
+  if (checkIfGameFinished(cells)) {
     removeAllEventClickListeners(cells);
   }
 }
@@ -70,20 +74,21 @@ function hideFailSelectedCouple(imagesSelected) {
   imagesSelected[1].parentElement.classList.add("grid-animals-cell_hover");
 }
 
-function checkIfGameFinished(cellsArray) {
+function checkIfGameFinished(cells) {
+  let cellsArray = Array.from(cells);
   let numberOfImagesDisplay = cellsArray.filter(cell => cell.children[0].style.display === "table-cell").length;
+
   if (numberOfImagesDisplay === cellsArray.length) {
     let containerDiv = document.querySelector(".container");
     let newH2Element = document.createElement("h2");
     newH2Element.textContent = "¡Juego finalizado!";
     containerDiv.appendChild(newH2Element);
-    
     return true;
   }
 }
 
 function removeAllEventClickListeners(cells) {
   for (let i = 0; i < cells.length; i++) {
-    cells[i].removeEventListener("click", selectAnimal);
+    cells[i].removeEventListener("click", flipCard);
   }
 }
