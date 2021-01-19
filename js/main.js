9c859885ae0f69a917d0c347de8d02f4b68f08b4
alan-garcia <alangarcianavarro@hotmail.com>
2021-01-19 14:40:20 +0100

    Aplicar diseño centrado para el tablero del juego

--- js/main.js
+++ js/main.js
@@ -31,25 +31,37 @@ function deleteRepeatedAnimal(imageName) {
   return imagesCopy;
 }
 
-cells.forEach(cell => {
-  cell.addEventListener("click", (event) => {
-    let currentCell = event.currentTarget;
-    let thisAnimal = currentCell.children[0];
-    
-    thisAnimal.style.display = "table-cell";
-    imagesSelected.push(thisAnimal);
-    currentCell.classList.remove("grid-animals-cell_hover");
+let lockBoard = false;
+cells.forEach(cell => cell.addEventListener("click", flipCard));
 
-    if (isCoupleSelected()) {
-      if (imagesSelected[0].src !== imagesSelected[1].src) {
-        setTimeout(() => {
-          hideFailSelectedCouple(imagesSelected);
-          imagesSelected = [];
-        }, 1200);
-      }
+function flipCard(event) {
+  if(lockBoard) {
+    return;
+  }
+  let currentCell = event.currentTarget;
+  let thisAnimal = currentCell.children[0];
+  
+  thisAnimal.style.display = "table-cell";
+  imagesSelected.push(thisAnimal);
+  currentCell.classList.remove("grid-animals-cell_hover");
+  if (isCoupleSelected()) {
+    lockboard = true;
+    if (imagesSelected[0].src !== imagesSelected[1].src) {
+      setTimeout(() => {
+        hideFailSelectedCouple(imagesSelected);
+        imagesSelected = [];
+        lockBoard = false;
+      }, 1200);
     }
-  });
-});
+    else {
+      imagesSelected = [];
+    }
+  }
+  
+  if (checkIfGameFinished(cells)) {
+    removeAllEventClickListeners(cells);
+  }
+}
 
 function isCoupleSelected() {
   return imagesSelected.length === 2;
@@ -60,4 +72,23 @@ function hideFailSelectedCouple(imagesSelected) {
   imagesSelected[0].parentElement.classList.add("grid-animals-cell_hover");
   imagesSelected[1].style.display = "none";
   imagesSelected[1].parentElement.classList.add("grid-animals-cell_hover");
+}
+
+function checkIfGameFinished(cells) {
+  let cellsArray = Array.from(cells);
+  let numberOfImagesDisplay = cellsArray.filter(cell => cell.children[0].style.display === "table-cell").length;
+
+  if (numberOfImagesDisplay === cellsArray.length) {
+    let containerDiv = document.querySelector(".container");
+    let newH2Element = document.createElement("h2");
+    newH2Element.textContent = "¡Juego finalizado!";
+    containerDiv.appendChild(newH2Element);
+    return true;
+  }
+}
+
+function removeAllEventClickListeners(cells) {
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener("click", flipCard);
+  }
 }
