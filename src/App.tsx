a5e92ee9ab182f635545b896cabed0dd913345b6
Alan García <alangarcianavarro@hotmail.com>
2024-06-08 19:12:55 +0200

    Pantalla de bienvenida para seleccionar el nivel de dificultad

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ import './App.css'
 function App() {
   return (
     <>
+      <div className="parejas-inicio-container">
+        <h1>Juego de parejas de animales</h1>
+        <p>Seleccione el nivel de dificultad:</p>
+        <div className="parejas-dificultad">
+          <div className="parejas-dificultad-recuadro dificultad-facil">Fácil</div>
+          <div className="parejas-dificultad-recuadro dificultad-medio">Medio</div>
+          <div className="parejas-dificultad-recuadro dificultad-dificil">Difícil</div>
+        </div>
+      </div>
     </>
   )
 }
