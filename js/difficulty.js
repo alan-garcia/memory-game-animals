cf4c816f0d22866a35d62443786779bd2569ac0d
Alan García <alangarcianavarro@hotmail.com>
2024-06-08 18:25:29 +0200

    Creación proyecto vacío con React 18 + TypeScript 5 + Vite

--- js/difficulty.js
+++
@@ -1,6 +0,0 @@
-export var Difficulty;
-(function (Difficulty) {
-    Difficulty["EASY"] = "easy";
-    Difficulty["MEDIUM"] = "medium";
-    Difficulty["HARD"] = "hard";
-})(Difficulty || (Difficulty = {}));
--- ts/difficulty.ts
+++
@@ -1,5 +0,0 @@
-export enum Difficulty {
-  EASY = "easy",
-  MEDIUM = "medium",
-  HARD = "hard"
-}
