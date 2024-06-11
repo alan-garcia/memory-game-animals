3eb16b34909d1158d8ffa6052f7568c4abe7f376
Alan García <alangarcianavarro@hotmail.com>
2024-06-11 14:50:34 +0200

    Implementación del juego de las parejas. Cargar imágenes animales.

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,159 @@
-import React, { useState } from 'react';
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import imagenes from './imagenes';
 
 function App() {
   const [openJugar, setOpenJugar] = useState(false);
-  const [celdas, setCeldas] = useState<JSX.Element[]>([]);
+  const [numFilas, setNumFilas] = useState(0);
+  const [numCeldas, setNumCeldas] = useState(0);
+  const [images, setImages] = useState<string[]>([]);
+  const [flippedIndices, setFlippedIndices] = useState<number[]>([]);
+  const [matchedIndices, setMatchedIndices] = useState<number[]>([]);
 
   const getDificultad = (event) => {
-    let numFilas = 0;
-    let numCeldas = 0;
-    const dificultad  = event.target.dataset.dificultad;
+    let filas = 0;
+    let celdas = 0;
+    const dificultad = event.target.dataset.dificultad;
 
     switch (dificultad) {
       case 'facil':
-        numFilas = 2;
-        numCeldas = 3;
+        filas = 2;
+        celdas = 3;
         break;
       case 'medio':
-        numFilas = 3;
-        numCeldas = 4;
+        filas = 3;
+        celdas = 4;
         break;
       case 'dificil':
-        numFilas = 5;
-        numCeldas = 6;
+        filas = 5;
+        celdas = 6;
         break;
       default:
         break;
     }
 
-    setCeldas(generarCeldas(numFilas, numCeldas));
-    setOpenJugar(true);
-  }
+    setNumFilas(filas);
+    setNumCeldas(celdas);
+    const numPairs = (filas * celdas) / 2;
 
-  const generarCeldas = (numFilas: number, numCeldas: number): JSX.Element[] => {
+    setImages(generateImagePairs(numPairs));
+    setOpenJugar(true);
+  };
+
+  const generateImagePairs = (numPairs: number) => {
+    const images: string[] = [];
+    for (let i = 0; i < numPairs; i++) {
+      const img: string = imagenes[i % imagenes.length].src;
+      images.push(img, img);
+    }
+    
+    return images.sort(() => Math.random() - 0.5);
+  };
+
+  const voltear = (event) => {
+    const indice = (event.target.firstChild === null) ? event.target.parentElement.firstChild.dataset.indice : event.target.firstChild.dataset.indice;
+    if (flippedIndices.length < 2 && !flippedIndices.includes(indice) && !matchedIndices.includes(indice)) {
+      event.target.classList.add("flipped");
+      setFlippedIndices((prev) => [...prev, indice]);
+    }
+    else if (flippedIndices.length === 2) {
+      const [firstIndex, secondIndex] = flippedIndices;
+      if (images[firstIndex] === images[secondIndex]) {
+        setMatchedIndices((prev) => [...prev, firstIndex, secondIndex]);
+        setFlippedIndices([]);
+        document.querySelectorAll(".celda")[firstIndex].classList.add("matched");
+        document.querySelectorAll(".celda")[secondIndex].classList.add("matched");
+      }
+      else {
+        setTimeout(() => {
+          flippedIndices.filter(ind => {
+            if (ind !== matchedIndices[ind]) {
+              document.querySelectorAll(".celda")[ind].classList.remove("flipped");
+            } 
+          });
+          setFlippedIndices([]);
+        }, 1000);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (flippedIndices.length === 2) {
+      const [firstIndex, secondIndex] = flippedIndices;
+      if (images[firstIndex] === images[secondIndex]) {
+        setMatchedIndices((prev) => [...prev, firstIndex, secondIndex]);
+        setFlippedIndices([]);
+      }
+      else {
+        setTimeout(() => {
+          flippedIndices.filter(ind => {
+            if (ind !== matchedIndices[ind]) {
+              document.querySelectorAll(".celda")[ind].classList.remove("flipped");
+            } 
+          });
+          setFlippedIndices([]);
+        }, 1000);
+      }
+    }
+  }, [flippedIndices, images, matchedIndices]);
+
+  const generarCeldas = () => {
     const filas: JSX.Element[] = [];
+    let indice = 0;
 
     for (let i = 0; i < numFilas; i++) {
       const celdas: JSX.Element[] = [];
-
       for (let j = 0; j < numCeldas; j++) {
-        celdas.push(<div key={`celda-${i}-${j}`} className="celda"></div>);
+        celdas.push(
+          <div key={`celda-${i}-${j}`}
+            className={`celda ${flippedIndices.includes(indice) || matchedIndices.includes(indice) ? 'flipped' : ''}`}
+            onClick={(event) => voltear(event)}
+          >
+            <div className="back" data-indice={indice} style={{ backgroundImage: `url(${images[indice]})` }}></div>
+          </div>
+        );
+        indice++;
       }
-      
       filas.push(
         <div key={`fila-${i}`} className="parejas-animales-fila">
           {celdas}
         </div>
       );
     }
-    
     return filas;
   };
 
-  const reiniciarPartida = () => setOpenJugar(false);
+  const reiniciarPartida = () => {
+    setOpenJugar(false);
+    setFlippedIndices([]);
+    setMatchedIndices([]);
+  };
 
   return (
     <>
-    { !openJugar ? (
-      <div className="parejas-inicio-container">
-        <h1>Juego de parejas de animales</h1>
-        <p>Seleccione el nivel de dificultad:</p>
-        <div className="parejas-dificultad">
-          <div className="parejas-dificultad-recuadro dificultad-facil" onClick={getDificultad} data-dificultad="facil">Fácil</div>
-          <div className="parejas-dificultad-recuadro dificultad-medio" onClick={getDificultad} data-dificultad="medio">Medio</div>
-          <div className="parejas-dificultad-recuadro dificultad-dificil" onClick={getDificultad} data-dificultad="dificil">Difícil</div>
+      {!openJugar ? (
+        <div className="parejas-inicio-container">
+          <h1>Juego de parejas de animales</h1>
+          <p>Seleccione el nivel de dificultad:</p>
+          <div className="parejas-dificultad">
+            <div className="parejas-dificultad-recuadro dificultad-facil" onClick={getDificultad} data-dificultad="facil">Fácil</div>
+
+            <div className="parejas-dificultad-recuadro dificultad-medio" onClick={getDificultad} data-dificultad="medio">Medio</div>
+
+            <div className="parejas-dificultad-recuadro dificultad-dificil" onClick={getDificultad} data-dificultad="dificil">Difícil</div>
+          </div>
         </div>
-      </div>
-    ) : (
-      <div className="parejas-inicio-container">
-        <h1>Juego de parejas de animales</h1>
-        <div className="parejas-animales-container">
-          {celdas}
+      ) : (
+        <div className="parejas-inicio-container">
+          <h1>Juego de parejas de animales</h1>
+          <div className="parejas-animales-container">{generarCeldas()}</div>
+          <button className="parejas-volver-menu" onClick={reiniciarPartida}>
+            Volver al menú
+          </button>
         </div>
-        <button className="parejas-volver-menu" onClick={reiniciarPartida}>Volver al menú</button>
-      </div>
-    )}
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/imagenes.tsx
@@ -0,0 +1,19 @@
+const imagenes = [
+    { id: 1, src: './src/images/leon.jpg', title: 'leon' },
+    { id: 2, src: './src/images/elefante.jpg', title: 'elefante' },
+    { id: 3, src: './src/images/vaca.jpg', title: 'vaca' },
+    { id: 4, src: './src/images/perro.jpg', title: 'perro' },
+    { id: 5, src: './src/images/mono.jpg', title: 'mono' },
+    { id: 6, src: './src/images/oveja.jpg', title: 'oveja' },
+    { id: 7, src: './src/images/buho.jpg', title: 'buho' },
+    { id: 8, src: './src/images/burro.jpg', title: 'burro' },
+    { id: 9, src: './src/images/caballo.jpg', title: 'caballo' },
+    { id: 10, src: './src/images/cerdo.jpg', title: 'cerdo' },
+    { id: 11, src: './src/images/gallo.jpg', title: 'gallo' },
+    { id: 12, src: './src/images/gato.jpg', title: 'gato' },
+    { id: 13, src: './src/images/pato.jpg', title: 'pato' },
+    { id: 14, src: './src/images/pavo.jpg', title: 'pavo' },
+    { id: 15, src: './src/images/pollito.jpg', title: 'pollito' }
+  ];
+  
+  export default imagenes;
